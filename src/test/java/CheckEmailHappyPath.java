4819719b128f4a4adea691e2c952024b78aa82e8
Liza Nikitenko <Liz.nikitenko2015@gmail.com>
2020-02-19 14:34:37 +0200

    Initial commit

---
+++ src/test/java/CheckEmailHappyPath.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+
+public class CheckEmailHappyPath {
+    @DataProvider
+    public Object[][] validEmail() {
+        return new Object[][]{
+                {"kateryna@gmail.com"},
+                {"kateryna23@gmail.com"},
+                {"roman25@gmail.com"},
+                {"roman!67@gmail.com"},
+                {"roman_55@mail.ru"},
+                {"roman@roman.com"},
+                {"_roman45@ukr.net"}
+        };
+    }
+    @Test(dataProvider="validEmail")
+    public void ValidEmail(String email) {
+        boolean valid = Email.isEmailCorrect(email);
+        Assert.assertEquals(true, valid);
+    }
+}
+
